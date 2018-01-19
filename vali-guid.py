3050918a331d73b521b4d3d7b6326cb1c1679ca5
Tim Forbes <timothy.forbes@canada.ca>
2018-01-19 15:20:06 -0500

    Made some initial comments about processing.

--- vali-guid.py
+++ vali-guid.py
@@ -1,3 +1,15 @@
 #!/usr/bin/env python
 
 # a program to report on the uid and gid states of users on the biocluster.
+
+# 1. create a list of users from somewhere, possibly /etc/passwd
+
+# 2. for each user in the list, get its gid and uid on biocluster
+
+# 3. for each user in the list, ask its gid and uid from active directory (samba, wbinfo)
+
+# domain appears to be AAFC-AAC, according to samba.
+
+# 4. report on the exceptions in the list e.g. biocluster gid != AD gid
+
+# 5. exit with a meaningful return code. zero for fully-matched.
