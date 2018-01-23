55f2552078453f4bab1bcfa40c10aa7dabd39aa7
forbestim <timothy.forbes@canada.ca>
2018-01-23 15:55:27 +0000

    There is a new file in the repo. It's called mysand.py

--- mysand.py
+++ mysand.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python
 
+# going to use this source file for some sandbox code. It will be thrown away.
+#
+# The other thing that is going on: I am learning to use git and github. So, while this all appears messy, the true intention is to become familiar with the tools, confront some hurdles and make some mistakes but ultimately get experience.
+
 import sys
 import os
 import subprocess
