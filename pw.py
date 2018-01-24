d9ebdd2d2be5c97c49307bf2761e1d175a22fa8d
forbestim <timothy.forbes@canada.ca>
2018-01-24 19:19:58 +0000

    a function definition

--- pw.py
+++ pw.py
@@ -4,11 +4,16 @@
 #
 # The other thing that is going on: I am learning to use git and github. So, while this all appears messy, the true intention is to become familiar with the tools, confront some hurdles and make some mistakes but ultimately get experience.
 
-import sys
-import os
+#import sys
+#import os
 import pwd
 import grp
-import subprocess
+#import subprocess
+
+def printAllUsers( ):
+  for u in pwd.getpwall():
+    print(u[0], u[2])
+
 
 uid = pwd.getpwuid(1000)
 print(uid)
@@ -22,5 +27,5 @@ for p in pwd.getpwall():
     print ( p[0], grp.getgrgid(p[3])[0] )
     print ( p )
 
-
+printAllUsers( )
 
