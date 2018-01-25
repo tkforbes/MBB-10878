30d08665f07ef0bfdb51b71f90ee6f87d0d31aa9
forbestim <timothy.forbes@canada.ca>
2018-01-25 19:34:29 +0000

    yaml stuff

--- myyaml.py
+++ myyaml.py
@@ -5,9 +5,13 @@
 # The other thing that is going on: I am learning to use git and github. So, while this all appears messy, the true intention is to become familiar with the tools, confront some hurdles and make some mistakes but ultimately get experience.
 
 import yaml
-
 with open("config.yaml", 'r') as stream:
         try:
-                    print(yaml.load(stream))
+                   cfg = yaml.safe_load(stream)
+                    xusers= cfg['excluded_users']
+                    print(cfg)
+                    print(xusers)
+                    print(xusers[2])
+
         except yaml.YAMLError as exc:
                     print(exc)
